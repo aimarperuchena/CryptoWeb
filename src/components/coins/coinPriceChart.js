dfa5ddff78ecd07f8c7954623f4c62ff59ca6964
aperuchena <aimarperuchena@gmail.com>
2020-05-13 14:02:03 +0200

    escala de grafico

--- src/components/coins/coinPriceChart.js
+++ src/components/coins/coinPriceChart.js
@@ -15,7 +15,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    height: 300,
+    height: 500,
     margin: 5,
     paddingRight: 20,
     width: '100%',
@@ -29,24 +29,37 @@ export default function PriceChart(props) {
   let days = props.days;
 
   const chartDataPreparation = () => {
+    let min=1000000;
+    let max=0;
     try {
       for (let i = 0; i < data.length; i++) {
         var myDate = new Date(data[i][0]);
         if (days > 1) {
           let time = myDate.toLocaleDateString();
-
+          if(data[i][1]>max){
+            max=data[i][1];
+          }
+          if(data[i][1]<min){
+            min=data[i][1];
+          }
           prices.push({ time: time, price: data[i][1] });
         } else {
           let time = time_format(myDate);
-
+          if(data[i][1]>max){
+            max=data[i][1];
+          }
+          if(data[i][1]<min){
+            min=data[i][1];
+          }
           prices.push({ time: time, price: data[i][1] });
         }
       }
+     /*  console.log('MAX: '+max+' MAX SUBIDO: '+max*1.1); */
       return (
         <ResponsiveContainer width="100%" height="80%">
           <AreaChart data={prices}>
             <XAxis dataKey="time" />
-            <YAxis />
+            <YAxis type="number" domain={['auto', 'auto']} />
             <CartesianGrid strokeDasharray="3 3" />
             <Tooltip cursor={{ stroke: 'red', strokeWidth: 2 }} />
             <Legend verticalAlign="top" height={36} />
